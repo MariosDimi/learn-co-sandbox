bb88c50284875eff4ae969c4cf7ec2cee3294f24
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2020-09-11 01:27:56 +0000

    Automatically backed up by Learn

--- movies-opening/lib/movies/opening.rb
+++
@@ -1,8 +0,0 @@
-require "movies/opening/version"
-
-module Movies
-  module Opening
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- movies-opening/lib/movies/opening/version.rb
+++
@@ -1,5 +0,0 @@
-module Movies
-  module Opening
-    VERSION = "0.1.0"
-  end
-end
